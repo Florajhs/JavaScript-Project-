9a5947f4402e5c8cf9a2567a65fc19c981a74d66
Florajhs <127689266+Florajhs@users.noreply.github.com>
2023-12-05 15:00:39 +0000

    Update JavaScript.js

--- JavaScript.js
+++ JavaScript.js
@@ -41,10 +41,3 @@ foodChoice.addEventListener("change", selectedChoice);
 
 
 
-
-
-// We missed </form> on HTML which helped solve the href issue.
-// We wanted to bring the submit button & the text to the center but this wasn't working in CSS so put this in HTML.
-// Adding in <br> to separate the lines. 
-// Great working as a team as everyone brought in different ideas/aspects to the coding. All have our own strengths which we put together.
-// If we had more time we would add in music, more detailed information about restaurant locations/recommendations. Have a drop down list of the restaurant recommendations which then would show a map & reviews. 
